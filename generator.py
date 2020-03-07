08ac45f2ecb889e33b57e5d53e89212fc86574f6
Quinn F <gqferbs@gmail.com>
2020-03-07 01:28:56 -0800

    Refactored to a more general model for generation Now supports generation of generic data based off a generation function and some input data. Also pandas is nice.

--- generator.py
+++ generator.py
@@ -1,23 +1,91 @@
-import pickle  # load kdf
-import csv
-import time  # for timestamp
+from typing import List, Dict, Callable
 
+import pandas as pd
 import numpy as np
-from scipy import stats
+import pickle
+import time
 
+
+def generate(
+    csv: str,
+    generator_dict: Dict[List[str], Callable[[pd.DataFrame], pd.DataFrame]],
+    default_func: Callable[[pd.DataFrame], pd.DataFrame] = lambda df: df.loc[
+        :, df.columns != "time"
+    ],
+    time_name: str = "time",
+) -> pd.DataFrame:
+    """
+    Generates data based off of a csv of real (or not) data.
+
+    Args:
+        csv: path to csv file of rocket data. Must at least include time entry.
+        generator_dict: dictionary of generator functions to be used for generating
+                        the data. Each function is passed the columns of the csv containing the headers supplied by the dict's key in a DataFrame
+                        along with the time column. Columns other than time can only be used by a generator function once.
+                        The columns of the DataFrame that are returned will be present in generate()'s return value along with time.
+        default_func: The default function used if generator_dict does not contain an entry for a column. default behaviour
+                      is to pass through columns untouched.
+        time_name: Name of the time column in the csv.
+
+    Returns:
+        A dataframe of the newly generated data.
+    """
+
+    input_frame = pd.read_csv(csv)
+    output_frame = input_frame[time_name].copy().to_frame()
+
+    # evaluate all generator functions
+    for headers, gen_func in generator_dict:
+        output_frame.merge(gen_func(input_frame[[time_name] + headers]))
+        output_frame.drop(columns=headers)
+
+    # call default function on any leftover columns
+    for column in [key for key in input_frame.keys() if key != time_name]:
+        output_frame = pd.concat(
+            [output_frame, default_func(input_frame[[time_name, column]])], axis=1
+        )
+        output_frame.drop(columns=column)
+
+    return output_frame
+
+
+# TODO: generate this on first run then cache as pickle
 KERNEL = pickle.load(open("output/BarometerKDE.pickle", "rb"))
 
 
-def uniform_random(mean: float, width: float) -> float:
-    """ Returns a random number in the uniform distribution [mean + width/2, mean - width/2] """
-    return mean + np.random.random_sample() * width - width / 2
+def gen_pres_from_alt(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generates pressure based on altitude. Takes a dataframe with columns time and alt_ft.
+    Returns dataframe with column pressure.
+    """
+    # NOTE: the time column is completely ignored
+    # TODO: possibly have the key set dynamically
+    out_series = df["alt_ft"].copy().rename_axis("pressure")
+    out_series *= 0.3048  # Convert to metres
+
+    # TODO: also possibly have these set dynamically
+    P_b = _uniform_random(1018, 4)  # mbar
+    # others should be SI units
+    L_b = _uniform_random(-0.006, 0.001)
+    T = _uniform_random(290, 20)
+
+    out_series["alt_ft"] = _alt_to_pres(
+        out_series["alt_ft"], P_b, L_b, T
+    )  # convert to mbar
+
+    out_series["alt_ft"] = out_series["alt_ft"].apply(lambda a: a + KERNEL.resample(1))
+    # Quantization
+    out_series["alt_ft"] = out_series["alt_ft"].apply(lambda a: _general_round(a, 0.01))
+
+    return out_series.to_frame()
 
 
-def alt_to_pres(alt: float, P_b: float, L_b: float, T: float) -> float:
-    """ returns pressure in mbar or Pa, depending on units of P_b.
-        alt is in meters,
-        P_b is pressure at sea level (Pa or mbar),
-        L_b is the standard temperature lapse rate (K/m).
+def _alt_to_pres(alt: float, P_b: float, L_b: float, T: float) -> float:
+    """
+    returns pressure in mbar or Pa, depending on units of P_b.
+    alt is in meters,
+    P_b is pressure at sea level (Pa or mbar),
+    L_b is the standard temperature lapse rate (K/m).
     """
     R = 8.31432  # universal gas constant - N*m/(mol*K)
     g = 9.808  # gravitational acceleration constant - m/s^2
@@ -28,43 +96,19 @@ def alt_to_pres(alt: float, P_b: float, L_b: float, T: float) -> float:
     return P_b * (1 + L_b / T * alt) ** ((g * M) / (R * L_b))
 
 
+# util functions
+def _uniform_random(mean: float, width: float) -> float:
+    """ Returns a random number in the uniform distribution [mean + width/2, mean - width/2] """
+    return mean + np.random.random_sample() * width - width / 2
+
+
 # add measurement errors
-def general_round(a: float, min_clip: float) -> float:
+def _general_round(a: float, min_clip: float) -> float:
     """ Rounds a to nearest min_clip. """
     return round(a / min_clip) * min_clip
 
 
-def run_file(filename: str) -> None:
-
-    raw_src_dat = np.genfromtxt(
-        f"input/{filename}.csv", dtype=np.unicode_, delimiter=","
-    )
-
-    # for now
-    assert raw_src_dat[0, 0] == "time_s"
-    assert raw_src_dat[0, 1] == "alt_ft"
-
-    src_dat = np.array(raw_src_dat[2:, :], dtype=np.float64)
-    src_dat[:, 1] *= 0.3048  # Convert to metres
-
-    P_b = uniform_random(1018, 4)  # mbar
-    # others should be SI units
-    L_b = uniform_random(-0.006, 0.001)
-    T = uniform_random(290, 20)
-
-    src_dat[:, 1] = alt_to_pres(src_dat[:, 1], P_b, L_b, T)  # convert to mbar
-
-    with open(
-        f"output/SkyPilotSimulation__gen_{time.time()}.csv", "w", newline=""
-    ) as out_file:
-        out_csver = csv.writer(out_file)
-        out_csver.writerow((3,))
-        out_csver.writerow((P_b, raw_src_dat[1, 0]))
-        for i in range(src_dat.shape[0]):
-            out_dat = src_dat[i, :]
-            out_dat[1] += KERNEL.resample(1)
-            out_dat[1] = general_round(out_dat[1], 0.01)  # Quantization
-            out_csver.writerow(src_dat[i, :])
-
-
-run_file("SkyPilotSimulation")
+# TODO: create a cli
+generate(
+    "input/test-data.csv", {"alt_ft": gen_pres_from_alt}, time_name="time_s"
+).to_csv(f"output/skypilot_gen_pres_{time.time()}.csv")
